dedfc2b7afce0f6c4a589ab4426aa02f6de9c7e5
Neshrack <hunter.walls61@gmail.com>
2023-02-20 15:35:44 -0500

    playing with tests

--- Lambda/app-test.py
+++ Lambda/app-test.py
@@ -1,38 +1,26 @@
 import boto3
 import unittest
-from moto import mock_dynamodb2
+from unittest.mock import MagicMock
 
 class TestLambdaFunction(unittest.TestCase):
 
-    @mock_dynamodb2
     def test_lambda_handler(self):
-        # Setup the mock DynamoDB table
-        dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-        table = dynamodb.create_table(
-            TableName='Visitors',
-            KeySchema=[
-                {
-                    'AttributeName': 'VisitorCount',
-                    'KeyType': 'HASH'
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'VisitorCount',
-                    'AttributeType': 'S'
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 5,
-                'WriteCapacityUnits': 5
+        # Mock the DynamoDB table
+        table_mock = MagicMock()
+        table_mock.get_item.return_value = {
+            'Item': {
+                'VisitorCount': '0'
             }
-        )
-        
+        }
+        dynamodb_mock = MagicMock()
+        dynamodb_mock.Table.return_value = table_mock
+        boto3.resource = MagicMock(return_value=dynamodb_mock)
+
         # Call the lambda function
         from dynamo_helper import lambda_handler
         response = lambda_handler(None, None)
-        
+
         # Check the response body and DynamoDB table
         expected_body = f'View count updated successfully. Total visitors: 1'
         self.assertEqual(response['body'], expected_body)
-        self.assertEqual(table.get_item(Key={'VisitorCount': 'vc'})['Item']['VisitorCount'], '1')
+        table_mock.put_item.assert_called_with(Item={'VisitorCount': '1'})
