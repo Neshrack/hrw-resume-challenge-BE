d915eb132d7f98e17a8788f2bdd1a24a7adfb10e
Hunter Walls <hunter.walls@gmail.com>
2023-02-20 20:43:06 -0500

    fixing my lambda

--- Lambda/dynamo_helper.py
+++ Lambda/dynamo_helper.py
@@ -1,26 +1,38 @@
 import boto3
+dynamo_client = boto3.resource('dynamodb')
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Visitors')
-
+def update_table(table, pk, column):
+    table = dynamo_client.Table(table)
     response = table.update_item(
-        Key={
-            'VisitorCount': 'vc'
-        },
-        UpdateExpression='SET VisitorCount = VisitorCount + :val',
-        ExpressionAttributeValues={
-            ':val': 1
-        }
+        Key={pk: 1},
+        UpdateExpression='ADD ' + column + ' :incr',
+        ExpressionAttributeValues={':incr': 1}
     )
 
-    total_visitors = table.get_item(
-        Key={
-            'VisitorCount': 'vc'
-        }
-    )['Item']['VisitorCount']
+    print(response)
+
+def get_count(table, pk, column):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamo_client.Table(table)
+    response = table.get_item(
+            Key={pk: 1}
+        )
+    count = response['Item'][column]
+    return(count)
+
+def lambda_handler(event, context):
+    update_table('Visitors', 'VisitorCount', 'vc')
+    get_count('Visitors', 'VisitorCount', 'vc')
+
+
 
     return {
-        'statusCode': 200,
-        'body': f'View count updated successfully. Total visitors: {total_visitors}'
+    'statusCode': 200,
+    'headers': { 
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",  # Allow only GET request
+        },
+    'body': get_count('Visitors', 'VisitorCount', 'vc')
+
     }
