833356137dbada92f4fb36dd23e4e6eed0b03d96
Hunter Walls <hunter.walls@gmail.com>
2023-02-20 22:05:56 -0500

    fixing my lambda

--- Lambda/dynamo_helper.py
+++ Lambda/dynamo_helper.py
@@ -1,12 +1,8 @@
 import boto3
-
 dynamo_client = boto3.resource('dynamodb')
-table_name = 'Visitors'
-pk = 'VisitorCount'
-column = 'vc'
 
-def update_table():
-    table = dynamo_client.Table(table_name)
+def update_table(table, pk, column):
+    table = dynamo_client.Table(table)
     response = table.update_item(
         Key={pk: 1},
         UpdateExpression='ADD ' + column + ' :incr',
@@ -15,24 +11,28 @@ def update_table():
 
     print(response)
 
-def get_count():
-    table = dynamo_client.Table(table_name)
+def get_count(table, pk, column):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamo_client.Table(table)
     response = table.get_item(
-        Key={pk: 1}
-    )
+            Key={pk: 1}
+        )
     count = response['Item'][column]
-    return count
+    return(count)
 
 def lambda_handler(event, context):
-    update_table()
-    count = get_count()
+    update_table('Visitors', 'VisitorCount', 'vc')
+    get_count('Visitors', 'VisitorCount', 'vc')
+
+
 
     return {
-        'statusCode': 200,
-        'headers': { 
-            "Access-Control-Allow-Headers": "*",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",  # Allow only GET request
+    'statusCode': 200,
+    'headers': { 
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",  # Allow only GET request
         },
-        'body': str(count)
+    'body': get_count('Visitors', 'VisitorCount', 'vc')
+
     }
